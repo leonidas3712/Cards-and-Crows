c296c6077dcf44125c8b5bd40efc0532d6f11145
omri <42713743+leonidas3712@users.noreply.github.com>
2022-09-24 19:30:53 +0300

    lane functionality

--- Assets/Scripts/LaneComponent.cs
+++ Assets/Scripts/LaneComponent.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class LaneComponent : MonoBehaviour
 {
-   
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public SlotComponent playerSlot,enemySlot;
+    public EntityComponent playerTarget,enemyTarget;
+
+    public MinionComponent playerMinion,enemyMinion;
+    public void Attack(){
+        if(playerSlot.current_minion)enemyTarget = playerSlot.current_minion;
+        if(enemySlot.current_minion)playerTarget = enemySlot.current_minion;
+
+        if(enemySlot.current_minion)
+            enemyTarget.Hit(enemySlot.current_minion.cardSO.attackStrength);
+        if(playerSlot.current_minion)
+            playerTarget.Hit(playerSlot.current_minion.cardSO.attackStrength);
     }
 }
